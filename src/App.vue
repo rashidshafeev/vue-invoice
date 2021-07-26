3b9ea66fc62a9fad28257f323005d6f74e338e6b
rashidshafeev <rashidshafeev@gmail.com>
2021-07-26 18:28:31 +0300

    Layout, cards

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,172 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <section class="header">
+      <div class="container"><h1>Тестовое задание</h1></div>
+    </section>
+    <section class="main">
+      <div class="left-column">
+        <button class="add-button"></button>
+        <div class="filters"></div>
+      </div>
+      <div class="right-column">
+        <div class="view-options-bar">
+          <div class="sorting-options-bar"></div>
+          <div class="view-buttons-wrapper">
+            <button
+              class="cards-view-button"
+              @click="switchToCardsView()">
+              <img src="@/assets/cards-view.svg" alt="">
+            </button>
+            <button
+              class="table-view-button"
+              @click="switchToTableView()">
+              <img src="@/assets/table-view.svg" alt="">
+            </button>
+          </div>
+        </div>
+        <router-view></router-view>
+      </div>
+    </section>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
 
-#nav {
-  padding: 30px;
+export default {
+  name: 'Home',
+  methods: {
+    switchToCardsView() {
+      this.$router.push({ path: '/cards' });
+    },
+    switchToTableView() {
+      this.$router.push({ path: '/table' });
+    },
+  },
+  components: {
+  },
+};
+</script>
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+<style lang="scss" scoped>
 
-    &.router-link-exact-active {
-      color: #42b983;
+  .container {
+    width: 1220px;
+  }
+
+  .header {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    height: 261px;
+
+    color: #FFFFFF;
+
+    background: linear-gradient(267.21deg, #900793 0.16%,
+    rgba(134, 4, 137, 0.785707) 0.16%,
+    rgba(159, 12, 162, 0.762269) 43.22%,
+    rgba(5, 1, 190, 0.734375) 98.12%,
+    rgba(0, 28, 126, 0.619792) 100.15%,
+    rgba(6, 77, 142, 0.447917) 102.03%,
+    rgba(1, 0, 60, 0) 102.73%);
+
+    h1 {
+      text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
     }
   }
-}
+
+  .main {
+    display: flex;
+    justify-content: center;
+    align-items: start;
+
+    padding-top: 30px;
+
+    background: #F8F8F8;
+  }
+
+  .left-column {
+    width: 280px;
+
+    margin-right: 30px;
+  }
+
+  .add-button {
+    width: 100%;
+    height: 70px;
+
+    margin-bottom: 20px;
+
+    background: #4943CD;
+    box-shadow: 0px 4px 4px #EBEBEB;
+    border: none;
+    border-radius: 2px;
+  }
+
+  .filters {
+    width: 100%;
+    height: 130px;
+
+    background: #FFFFFF;
+    box-shadow: 0px 4px 8px rgba(179, 178, 178, 0.25)
+  }
+
+  .right-column {
+    width: 912px;
+  }
+
+  .view-options-bar {
+    display: flex;
+
+    width: 100%;
+    height: 70px;
+
+    margin-bottom: 20px;
+  }
+
+  .sorting-options-bar {
+    width: 810px;
+    height: 100%;
+
+    background: #FFFFFF;
+    box-shadow: 0px 4px 12px #E7E7E7;
+    border-radius: 3px;
+  }
+
+  .view-buttons-wrapper {
+    width: 102px;
+
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+  }
+
+  .cards-view-button {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    width: 40px;
+    height: 40px;
+
+    background: #FFFFFF;
+
+    border: 1px solid #647A8C;
+    border-radius: 4px 0 0 4px;
+  }
+
+  .table-view-button {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    width: 40px;
+    height: 40px;
+
+    background: #FFFFFF;
+
+    border: 1px solid #647A8C;
+    border-radius: 0 4px 4px 0;
+  }
+
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,20 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
+import Home from '../views/Cards.vue';
+import Table from '../views/Table.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
+    path: '/cards',
+    name: 'Cards',
     component: Home,
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    path: '/table',
+    name: 'Table',
+    component: Table,
   },
 ];
 
